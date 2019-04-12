354263a70f4ebbf6865cf488567519b08c951621
omobosteven <omobosteven@gmail.com>
2019-04-12 22:53:17 +0100

    ch(setup eslint): setup linting tool [finishes #165324341]

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+
 const app = express();
 const port = 3000;
 
@@ -6,8 +7,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.status(200).json({
-    status: 200,
-    message: 'Turing App!!!'
+  status: 200,
+  message: 'Turing App!!!'
 }));
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
