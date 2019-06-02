9d648995d263fae4d36365b2b993c2f73dfdb809
omobosteven <omobosteven@gmail.com>
2019-06-02 19:47:44 +0100

    ft(attributes routes): create api endpoints for attribute - get all attributes - get attributes by Id - get attributes value from attributes - get attributes for a product [finishes #166399930]

---
+++ src/routes/api/attributes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import AttributeController from '../../controllers/AttributeController';
+
+const attributes = express.Router();
+
+attributes.get('/', AttributeController.getAttributes);
+attributes.get('/:attribute_id', AttributeController.getAttributeById);
+attributes.get('/values/:attribute_id', AttributeController.getValueOfAttributes);
+attributes.get('/inProduct/:product_id', AttributeController.getAttributesOfproduct);
+
+export default attributes;
