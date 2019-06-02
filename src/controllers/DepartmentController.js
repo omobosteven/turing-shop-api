53eda37ce99753752764ea3ce0316adb7f537fba
omobosteven <omobosteven@gmail.com>
2019-06-02 16:19:46 +0100

    ft(department routes): create api endpoints for department - get all departments - get department by Id [finishes #165480486]

---
+++ src/controllers/DepartmentController.js
@@ -0,0 +1,49 @@
+import db from '../db/models';
+
+const { sequelize } = db;
+
+class DepartmentController {
+  /**
+   * Get all departments
+   * @param {*} req - query parameters
+   * @param {*} res - Response object
+   * @param {*} next - Next function
+   * @returns {object} departments - Department object
+   */
+  static getDepartments(req, res, next) {
+    sequelize
+      .query('CALL catalog_get_departments()')
+      .then(departments => res.status(200).send(departments))
+      .catch(err => next(err.message));
+  }
+
+  /**
+   * Get department by Id
+   * @param {*} req - query parameters
+   * @param {*} res - Response object
+   * @param {*} next - Next function
+   * @returns {object} department - Department object
+   */
+  static getDepartmentById(req, res, next) {
+    const { department_id } = req.params;
+
+    sequelize
+      .query(`CALL catalog_get_department_details(${department_id})`)
+      .then((department) => {
+        if (!department[0]) {
+          return res.status(404).send({
+            error: {
+              status: 404,
+              code: 'DEP_02',
+              message: 'Don\'exist department with this ID.',
+              field: 'department_id'
+            }
+          });
+        }
+        return res.status(200).send(department[0]);
+      })
+      .catch(err => next(err.message));
+  }
+}
+
+export default DepartmentController;
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import customers from './customers';
 import customer from './customer';
 import products from './products';
+import departments from './departments';
 
 const api = express.Router();
 
 api.use('/customer', customer);
 api.use('/customers', customers);
 api.use('/products', products);
+api.use('/departments', departments);
 
 export default api;
